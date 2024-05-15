26dde80170c1f24626065b51b6feb2f8ebb48cd4
chonky1234 <118490337+chonky1234@users.noreply.github.com>
2024-05-16 01:32:01 +0200

    startup testiranje

--- matura/Form1.cs
+++ matura/Form1.cs
@@ -29,13 +29,45 @@ namespace matura
             WindowState = FormWindowState.Maximized;
 
 
+
+            string sourcePath = "D:\\PROGRAMIRANJE\\C#\\cuvanje-ucenika\\matura\\bin\\Debug\\testfolder";
+            string targetPath = Environment.GetFolderPath(Environment.SpecialFolder.Startup);
+            string fileName = string.Empty;
+            string destFile = string.Empty;
+
+
+
+
+
             //StreamWriter sw = new StreamWriter("%PROGRAMDATA%\\Microsoft\\Windows\\Start Menu\\Programs\\Startup\\test.txt");
             //"%PROGRAMDATA%\\Microsoft\\Windows\\Start Menu\\Programs\\Startup"
             //StreamWriter sw = new StreamWriter("C:\\" + Environment.GetEnvironmentVariable("%PROGRAMDATA%") + "\\Microsoft\\Windows\\Start Menu\\Programs\\Startup\\test.txt");
             StreamWriter sw = new StreamWriter(Environment.GetFolderPath(Environment.SpecialFolder.Startup) + "\\test.txt", true);
             sw.WriteLine("blabla");
             sw.Close();
-            string s = Environment.GetFolderPath(Environment.SpecialFolder.Startup);
+            string provera = Environment.GetFolderPath(Environment.SpecialFolder.Startup);
+            //File.Copy("D:\\PROGRAMIRANJE\\C#\\cuvanje-ucenika\\matura\\bin\\Debug\\testfolder", Environment.GetFolderPath(Environment.SpecialFolder.Startup));
+
+
+            if (System.IO.Directory.Exists(sourcePath))
+            {
+                string[] files = System.IO.Directory.GetFiles(sourcePath);
+
+                // Copy the files and overwrite destination files if they already exist. 
+                foreach (string s in files)
+                {
+                    // Use static Path methods to extract only the file name from the path.
+                    fileName = System.IO.Path.GetFileName(s);
+                    destFile = System.IO.Path.Combine(targetPath, fileName);
+                    System.IO.File.Copy(s, destFile, true);
+                }
+            }
+            else
+            {
+                MessageBox.Show("Source path does not exist!");
+            }
+
+
         }
 
         private void btUnesiUcenika_Click(object sender, EventArgs e)
